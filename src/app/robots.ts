17a984699c6198e3ff357697bb7fc1e174da391e
bishalahmednirob10-stack <bishalahmednirob10-stack@users.noreply.github.com>
2026-06-07 00:58:44 +0600

    Update project files

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,3 +10,8 @@ export default function robots(): MetadataRoute.Robots {
     sitemap: `${brand.siteUrl}/sitemap.xml`,
   };
 }
+cd "c:/Users/Bishal/Documents/Codex/2026-05-28/https-chatgpt-com-share-6a17bad9-dc8c"
+git remote remove haha
+git remote add haha git@github.com:bishalahmednirob1/haha.git
+git push haha --all
+git push haha --tags
